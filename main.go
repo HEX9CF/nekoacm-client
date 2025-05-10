fc74644d2d80ed7617b352aa416a448c8872ca70
HEX9CF <hex9cf@aliyun.com>
2025-05-10 17:02:47 +0800

    add gRPC service and protobuf definitions for problem management

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"neko-acm-client/pkg/pb"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	dial, err := grpc.Dial("127.0.0.1:14516", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
 	}
+	defer dial.Close()
+
+	problemClient := pb.NewProblemServiceClient(dial)
+
+	pi := &pb.ProblemInstructionRequest{
+		Title: "Hello World",
+	}
+
+	resp, err := problemClient.GenerateProblem(context.Background(), pi)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v", resp)
 }
