74fffa62decc79ffb399a5f76abde68edbf50212
HEX9CF <hex9cf@aliyun.com>
2025-05-12 12:02:52 +0800

    add timeout configuration to NekoGrpcConf and config.yaml

--- pkg/config/neko.go
+++ pkg/config/neko.go
@@ -1,13 +1,15 @@
 package config
 
 type NekoGrpcConf struct {
-	Host  string `yaml:"host" json:"host"`
-	Port  string `yaml:"port" json:"port"`
-	Token string `yaml:"token" json:"token"`
+	Host    string `yaml:"host" json:"host"`
+	Port    string `yaml:"port" json:"port"`
+	Token   string `yaml:"token" json:"token"`
+	Timeout int    `yaml:"timeout" json:"timeout"`
 }
 
 func (g *NekoGrpcConf) Default() {
 	g.Host = "localhost"
 	g.Port = "14516"
 	g.Token = "token"
+	g.Timeout = 60
 }
