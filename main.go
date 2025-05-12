8889eab7e49580a80e62e064f752308ccd14b5d7
HEX9CF <hex9cf@aliyun.com>
2025-05-12 14:14:29 +0800

    add main function to initialize bootstrap and execute CLI commands

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"nekoacm-client/cmd/bootstrap"
+	"nekoacm-client/cmd/cli"
+)
+
+func main() {
+	err := bootstrap.Init()
+	if err != nil {
+		panic(err)
+	}
+	cli.Execute()
+}
